4981375aed89e7383f0a19e4f91511538d2ee2d1
david_smith <david_smith@sweetwater.com>
2025-01-28 10:46:16 -0500

    feat/cli Updated `src/SrcCommand.php`.

--- src/SrcCommand.php
+++ src/SrcCommand.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace Zerotoprod\:namespace;
+namespace Zerotoprod\ValidateSemverCli;
 
 use Symfony\Component\Console\Attribute\AsCommand;
 use Symfony\Component\Console\Command\Command;
@@ -13,11 +13,11 @@ use Symfony\Component\Console\Output\OutputInterface;
 )]
 class SrcCommand extends Command
 {
-    public const signature = ':slug:src';
+    public const signature = 'validate-semver-cli:src';
 
     protected function execute(InputInterface $input, OutputInterface $output): int
     {
-        $output->writeln('https://github.com/zero-to-prod/:slug');
+        $output->writeln('https://github.com/zero-to-prod/validate-semver-cli');
 
         return Command::SUCCESS;
     }
